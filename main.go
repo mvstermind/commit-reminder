1e646bce45323dd2800ca075814e25435259382d
mvstermind <zablockidawid1@gmail.com>
2024-07-25 11:31:49 +0200

    playing with parsing commit time

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	time := ParseArgs()
 	fmt.Println(time)
+	status := commitStatus(time)
+	fmt.Println(status)
+
+}
+
+func commitStatus(committime time.Time) bool {
+
+	if time.Since(committime) >= time.Hour {
+		fmt.Println("commited more than hour ago")
+		return true
+	}
+
+	return false
 }
