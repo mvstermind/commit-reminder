5b537a897323dad04944dbe68693a342d43edcf8
mvstermind <zablockidawid1@gmail.com>
2024-07-26 23:42:46 +0200

    todo: make sure that it will only count actuall commits

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -13,21 +13,21 @@ import (
 // ParseArgs func reads sys args passed during program call.
 // If proper flag for username is passed "-u" sends request to api.github
 // returns latest commit date
-func ParseArgs() (time.Time, error) {
+func ParseArgs() ([]time.Time, error) {
 
 	if len(os.Args) <= 1 {
-		return time.Time{}, errors.New("use -h or --help")
+		return []time.Time{}, errors.New("use -h or --help")
 	}
 	username := flag.String("u", "", "github username")
+
 	flag.Parse()
 
 	link := request.FormUrl(*username)
 	jsonTime, err := request.Send(link)
 
 	if err != nil {
-		return time.Time{}, err
+		return []time.Time{}, err
 	}
-	latestCommit := jsonTime[0]
-	return latestCommit, nil
+	return jsonTime, nil
 
 }
--- main.go
+++ main.go
@@ -15,22 +15,33 @@ func main() {
 		log.Printf("err: %v", err)
 		return
 	}
-	_ = commitTdayStatus(cmtTime)
+	commitTdayStatus(cmtTime)
 
 }
 
 // Validates checks if person commited tday returning true if commit was today
 // and false if person hasn't commited yet
-func commitTdayStatus(commitTime time.Time) bool {
-	commitTimeDay := commitTime.Day()
+func commitTdayStatus(commitTime []time.Time) {
+	commitTimeDay := commitTime[0].Day()
 	currDay := time.Now().Day()
 
-	if commitTimeDay == currDay {
-		fmt.Println("You have commited tday!!")
-		return true
+	for _, v := range commitTime {
+		fmt.Println(v)
+
+	}
+
+	emoji := '\U0001F389'
+	if commitTimeDay == currDay && len(commitTime) == 1 {
+		fmt.Printf("\n%c  You have commited tday !! %c\n\n", emoji, emoji)
+		return
+	}
+
+	if commitTimeDay == currDay && len(commitTime) > 1 {
+		fmt.Printf("\n%c  You have commited tday: %v times!! %c\n\n", emoji, len(commitTime), emoji)
+		return
+
 	}
 
 	fmt.Println("You haven't commited tday :(")
-	return false
 
 }
