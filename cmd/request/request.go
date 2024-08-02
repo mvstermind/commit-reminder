5ea1a1a5d7037eec66ea677928ba37da6aba8af4
mvstermind <zablockidawid1@gmail.com>
2024-08-02 20:30:33 +0200

    feat: running with username flag displays amount of commits in github that were commited today

--- cmd/request/request.go
+++ cmd/request/request.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+	"time"
 )
 
 type Event struct {
@@ -24,23 +26,43 @@ func Get(username string) error {
 		return err
 	}
 
+	// Fill in json response into slice
 	var events []Event
-
 	err = json.Unmarshal(bytebody, &events)
 	if err != nil {
 		return err
 	}
 
-	for _, event := range events {
-		fmt.Println(event)
+	// Modify json response to match format of currentDate (declared in howManyFunc)
+	var refactoredDate []string
+	for _, value := range events {
+		valid := strings.Split(value.CreatedAt, "T")
+		refactoredDate = append(refactoredDate, valid[0])
+
 	}
+	todayCommmits := commitsToday(refactoredDate)
 
+	fmt.Println(todayCommmits)
 	return nil
-
 }
 
 func formUrl(uname string) string {
 	url := fmt.Sprintf("https://api.github.com/users/%v/events", uname)
 	return url
+
+}
+
+func commitsToday(commits []string) int {
+	currentDate := time.Now().Format("2006-01-02")
+
+	var commitsToday int
+
+	for _, commit := range commits {
+		if commit == currentDate {
+			commitsToday++
+		}
+	}
+
+	return commitsToday
 
 }
